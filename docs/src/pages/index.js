2632d0ef295df541994758fa1e802cb74e97d880
Andreas Hebeisen <a.hebeisen@outlook.com>
2025-01-29 23:37:04 +0100

    fix getting started link

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/installation">
+            to="/docs/category/erste-schritte">
             Installation & Konfiguration - 5min ⏱️
           </Link>
         </div>
