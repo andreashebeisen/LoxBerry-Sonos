060d0d7defeea7510a04c4ac7a4d28f4f393202b
Andreas Hebeisen <a.hebeisen@outlook.com>
2025-01-31 02:51:40 +0100

    Update editUrl in docusaurus.config.js to point to the correct repository

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/andreashebeisen/LoxBerry-Sonos/tree/feature/docs/docs',
         },
         // blog: {
         //   showReadingTime: true,
